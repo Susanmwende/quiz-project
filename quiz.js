0de209d9d4ccdf4eba2d8c5dd343fc30d72a4372
Susanmwende <mwendezmasaku@gmail.com>
2024-05-19 18:20:58 +0300

    quiz project

---
+++ quiz.js
@@ -0,0 +1,15 @@
+const roundIcon = document.getElementById('roundicon1')
+roundIcon.style.cursor = "pointer"
+
+
+
+
+
+roundIcon.addEventListener('click',function(){   
+    roundIcon.style.color='blue'
+    roundIcon.innerHTML ="blue"
+
+
+
+
+})
